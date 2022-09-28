2c0b1692b7f8ac545e981eceaf2582b8e3ce4f3a
RayaneA7 <ka_kebir@esi.dz>
2022-09-28 13:39:15 +0100

    set up redux toolkit and json server

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
